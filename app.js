95b0ba7cd408c33363a87838d882628a9f3742b9
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-09 17:00:25 +0100

    connect the app with db and create middlewares

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+//async errors;
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send(
+    '<h1>store Api </h1> <a href="/api/v1/products">¨Products routes</a>',
+  );
+});
+
+const port = process.env.Port || 5000;
+//products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+async function start() {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listenning port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();

535bf187d057cf23066ae571d21aae2a2e35ced7
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-12 01:48:17 +0100

    formatting the controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,9 @@
+const Product = require("../models/product");
+
 const AllProductsStatic = async (req, res) => {
-  // throw new Error("testing async errors");
-  res.status(200).json({ msg: "products testing" });
+  const Products = await Product.find({ featured: false });
+
+  res.status(200).json({ msg: Products, nbHits: Products.length });
 };
 const AllProducts = async (req, res) => {
   res.status(200).json({ msg: "products route" });

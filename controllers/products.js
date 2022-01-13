66dcebb0b4a923944f99d64b109eefefb0c028d7
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-13 18:31:15 +0100

    add some logic in the controller

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,14 @@ const AllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: Products, nbHits: Products.length });
 };
 const AllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured } = req.query;
+  const objectQuery = {};
+  if (featured) {
+    objectQuery.featured = featured === "true" ? true : false;
+  }
+
+  const Products = await Product.find(objectQuery);
+  res.status(200).json({ msg: Products });
 };
 
 module.exports = {

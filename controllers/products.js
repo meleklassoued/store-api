35350403bedd73563ace8e0bad7b179f43a7349b
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-14 23:56:07 +0100

    add company query

--- controllers/products.js
+++ controllers/products.js
@@ -6,10 +6,13 @@ const AllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: Products, nbHits: Products.length });
 };
 const AllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, comapny } = req.query;
   const objectQuery = {};
   if (featured) {
     objectQuery.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.comapny = comapny;
   }
 
   const Products = await Product.find(objectQuery);

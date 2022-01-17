c281e5631629533f37ab67f7f2f268f78e15c559
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-17 01:14:14 +0100

    change sme objectqueries and add name to The objct

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,23 @@
 const Product = require("../models/product");
 
 const AllProductsStatic = async (req, res) => {
-  const Products = await Product.find({ featured: false });
+  const search = "a";
+  const Products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
 
   res.status(200).json({ msg: Products, nbHits: Products.length });
 };
 const AllProducts = async (req, res) => {
-  const { featured, comapny } = req.query;
+  const { featured, company, name } = req.query;
   const objectQuery = {};
-  if (featured) {
-    objectQuery.featured = featured === "true" ? true : false;
-  }
-  if (company) {
-    queryObject.comapny = comapny;
-  }
 
+  if (featured && company && name) {
+    objectQuery.featured = featured;
+    objectQuery.company = company;
+    objectQuery.name = { $regex: name, $options: "i" };
+  }
+  console.log(objectQuery);
   const Products = await Product.find(objectQuery);
   res.status(200).json({ msg: Products });
 };
